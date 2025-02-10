f293d66c44b6fd845a26825c6941ea6c67aa062f
kyle-sutherland <velveteen.rever@gmail.com>
2025-02-10 07:11:14 -0500

    new assets

--- app/page.tsx
+++ app/page.tsx
@@ -48,54 +48,65 @@ export default function Home() {
       </div>
       <div className="justify-center">
         <div className=" bg-emerald-950">
-          <div className="max-w-7xl mx-auto px-4 py-24 text-lg">
-            <p className="pb-10">
-              Tree Workers’ Industrial Group is a grassroots advocacy group that
-              is pushing for worker representation in the silviculture industry.
-              Founded in October of 2018, T.W.I.G. has grown to include members
-              spanning the range of colonially-called Canada and has been
-              involved in collective action at multiple planting companies –
-              advocating and promoting the legal rights of workers, developing
-              policies and awareness of sexual violence for tree-planting
-              companies, and returning over $60,000 of stolen wages to the
-              pockets of workers.
-            </p>
-            <p className="pb-10">
-              Our mission is to support workers from the bottom up and to build
-              solidarity and representation across company lines. We are
-              volunteer-run and are spearheading a sectoral culture shift toward
-              worker empowerment and transparency. We aim to educate and empower
-              workers to both support each other and be critical of the
-              colonial, patriarchal and capitalist aspects of this industry and
-              the harm they cause.
-            </p>
-            <p className="pb-10">* * * En français * * *</p>
-            <p className="pb-10">
-              Tree Workers’ Industrial Group (Groupe industriel des
-              travailleuses et travailleurs du reboisement) est un groupe de
-              défense d’intérêts par et pour les travailleuses et travailleurs
-              de l’industrie silvicole. Fondé en octobre 2018, TWIG a depuis
-              réuni des membres dans l’ensemble du soi-disant Canada et a été
-              impliqué dans des actions collectives dans plusieurs compagnies de
-              reboisement – faisant connaître et reconnaître les droits des
-              travailleuses et travailleurs, développant des politiques et une
-              sensibilisation à la violence sexuelle dans l’industrie, en plus
-              de remettre plus de 60 000 $ en salaires volés dans les poches de
-              travailleuses et travailleurs !
-            </p>
-            <p className="pb-10">
-              Notre mission est de soutenir les travailleuses et travailleurs à
-              partir de la base et de construire une solidarité et une
-              représentation au-delà des frontières entre les entreprises. Notre
-              groupe est autogéré bénévolement par ses membres et nous
-              souhaitons être le fer de lance d’un changement de culture
-              sectoriel encourageant la transparence et la reprise de pouvoir
-              par les travailleuses et travailleurs. Nous visons à se co-éduquer
-              et à nous donner les moyens, en tant que travailleuses et
-              travailleurs, de nous soutenir mutuellement et de critiquer les
-              aspects coloniaux, patriarcaux et capitalistes de cette industrie,
-              notamment en reconnaissant les dommages qu’ils causent.
-            </p>
+          <div className="max-w-7xl flex flex-col md:flex-row mx-auto px-4 py-24 text-lg space-x-6">
+            <div className="w-3/4">
+              <p className="pb-10">
+                Tree Workers’ Industrial Group is a grassroots advocacy group
+                that is pushing for worker representation in the silviculture
+                industry. Founded in October of 2018, T.W.I.G. has grown to
+                include members spanning the range of colonially-called Canada
+                and has been involved in collective action at multiple planting
+                companies – advocating and promoting the legal rights of
+                workers, developing policies and awareness of sexual violence
+                for tree-planting companies, and returning over $60,000 of
+                stolen wages to the pockets of workers.
+              </p>
+              <p className="pb-10">
+                Our mission is to support workers from the bottom up and to
+                build solidarity and representation across company lines. We are
+                volunteer-run and are spearheading a sectoral culture shift
+                toward worker empowerment and transparency. We aim to educate
+                and empower workers to both support each other and be critical
+                of the colonial, patriarchal and capitalist aspects of this
+                industry and the harm they cause.
+              </p>
+              <p className="pb-10">* * * En français * * *</p>
+              <p className="pb-10">
+                Tree Workers’ Industrial Group (Groupe industriel des
+                travailleuses et travailleurs du reboisement) est un groupe de
+                défense d’intérêts par et pour les travailleuses et travailleurs
+                de l’industrie silvicole. Fondé en octobre 2018, TWIG a depuis
+                réuni des membres dans l’ensemble du soi-disant Canada et a été
+                impliqué dans des actions collectives dans plusieurs compagnies
+                de reboisement – faisant connaître et reconnaître les droits des
+                travailleuses et travailleurs, développant des politiques et une
+                sensibilisation à la violence sexuelle dans l’industrie, en plus
+                de remettre plus de 60 000 $ en salaires volés dans les poches
+                de travailleuses et travailleurs !
+              </p>
+              <p className="pb-10">
+                Notre mission est de soutenir les travailleuses et travailleurs
+                à partir de la base et de construire une solidarité et une
+                représentation au-delà des frontières entre les entreprises.
+                Notre groupe est autogéré bénévolement par ses membres et nous
+                souhaitons être le fer de lance d’un changement de culture
+                sectoriel encourageant la transparence et la reprise de pouvoir
+                par les travailleuses et travailleurs. Nous visons à se
+                co-éduquer et à nous donner les moyens, en tant que
+                travailleuses et travailleurs, de nous soutenir mutuellement et
+                de critiquer les aspects coloniaux, patriarcaux et capitalistes
+                de cette industrie, notamment en reconnaissant les dommages
+                qu’ils causent.
+              </p>
+            </div>
+            <div className="flex w-1/4 items-center justify-center">
+              <Image
+                src="/logo/twig-logo-transparent-white.png"
+                alt="Twig logo"
+                width={300}
+                height={100}
+              />
+            </div>
           </div>
         </div>
         <div className="h-80 w-full">
